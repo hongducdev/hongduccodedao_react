65eede24f45532da17e57348a7c30fd481de86fb
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-12-03 18:50:29 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import "./App.scss";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Qualification from "./components/Qualification/Qualification";
-import Services from "./components/Services/Services";
-import Skills from "./components/Skills/Skills";
+// lazy load
+import { lazy, Suspense } from "react";
+const Header = lazy(() => import("./components/Header/Header"));
+const Home = lazy(() => import("./components/Home/Home"));
+const About = lazy(() => import("./components/About/About"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const Qualification = lazy(() =>
+   import("./components/Qualification/Qualification")
+);
+const Services = lazy(() => import("./components/Services/Services"));
+const Skills = lazy(() => import("./components/Skills/Skills"));
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            
-            <main className="main">
-                <Home />
-                <About />
-                <Skills />
-                <Services />
-                <Qualification />
-                <Contact />
-                <Footer />
-            </main>
-        </div>
-    );
+   return (
+      <div className="App">
+         <Suspense fallback={<div>Loading...</div>}></Suspense>
+         <Header />
+
+         <main className="main">
+            <Home />
+            <About />
+            <Skills />
+            <Services />
+            <Qualification />
+            <Contact />
+            <Footer />
+         </main>
+      </div>
+   );
 }
 
 export default App;
