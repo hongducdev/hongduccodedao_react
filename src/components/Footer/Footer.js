194c44382f0ae3166c760c6d6199f4c44e3d92d5
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-12-04 09:21:15 +0700

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,46 +4,46 @@ import './Footer.scss';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer__container container">
-                <h1 className="footer__title">hongduccodedao</h1>
+       <footer className="footer">
+          <div className="footer__container container">
+             <h1 className="footer__title">{'<hongduccodedao/>'}</h1>
 
-                <ul className="footer__list">
-                    <li>
-                        <a href="#about" className="footer__link">
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#portfolio" className="footer__link">
-                            Projects
-                        </a>
-                    </li>
-                </ul>
+             <ul className="footer__list">
+                <li>
+                   <a href="#about" className="footer__link">
+                      About
+                   </a>
+                </li>
+                <li>
+                   <a href="#portfolio" className="footer__link">
+                      Projects
+                   </a>
+                </li>
+             </ul>
 
-                <div className="footer__social">
-                    <a
-                        href="https://www.instagram.com/pinkduwc._/"
-                        className="footer__social-link">
-                        <i className="uil uil-instagram"></i>
-                    </a>
-                    <a
-                        href="https://github.com/hongduccodedao"
-                        className="footer__social-link">
-                        <i className="uil uil-github-alt"></i>
-                    </a>
-                    <a
-                        href="https://www.facebook.com/hongduccodedao.cf"
-                        className="footer__social-link">
-                        <i className="uil uil-facebook-f"></i>
-                    </a>
-                </div>
+             <div className="footer__social">
+                <a
+                   href="https://www.instagram.com/pinkduwc._/"
+                   className="footer__social-link">
+                   <i className="uil uil-instagram"></i>
+                </a>
+                <a
+                   href="https://github.com/hongduccodedao"
+                   className="footer__social-link">
+                   <i className="uil uil-github-alt"></i>
+                </a>
+                <a
+                   href="https://www.facebook.com/hongduccodedao.cf"
+                   className="footer__social-link">
+                   <i className="uil uil-facebook-f"></i>
+                </a>
+             </div>
 
-                <span className="footer__copyright">
-                    © 2021 hongduccodedao. All rights reserved.
-                </span>
-            </div>
-        </footer>
+             <span className="footer__copyright">
+                © 2021 hongduccodedao. All rights reserved.
+             </span>
+          </div>
+       </footer>
     );
 };
 
