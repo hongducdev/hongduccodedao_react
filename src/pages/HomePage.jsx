1b4f38a8e82fea5c9f7ca87a24b0660ae9511a69
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-01-27 16:53:05 +0700

    🐛Fix height HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const HomePage = () => {
   return (
     <Fragment>
-      <div className="bg-lite dark:bg-darkbg h-full">
+      <div className="bg-lite dark:bg-darkbg h-screen">
         <div className="flex items-center justify-center h-full w-full text-center">
           <h1 className='text-text1 dark:text-grayf3 font-bold text-8xl relative after:absolute after:w-[10%] after:h-2 after:bg-text1 after:dark:bg-grayf3 after:bottom-0 after:animate-pulse2 block'>
             Hello World
