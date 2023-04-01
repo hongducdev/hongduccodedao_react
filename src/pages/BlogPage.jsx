3c626ec6616f8df763ea9458e226333a0c43ad07
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-04-01 14:56:09 +0700

    Update Loading for Blog

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -2,21 +2,24 @@ import React, { Fragment, useEffect, useState } from "react";
 import HeadingPage from "../components/HeadingPage";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 const BlogPage = () => {
   // change title
   document.title = "Blogs | hongduccodedao";
 
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         "https://backendhongduccodedao-production.up.railway.app/api/v1/posts"
       )
       .then((res) => {
-        console.log(res.data);
         setBlogs(res.data);
+        setLoading(false);
       });
   }, []);
 
@@ -26,20 +29,34 @@ const BlogPage = () => {
         <div className="max-w-[1200px] mx-auto px-5 lg:px-0">
           <HeadingPage title="My Blogs"></HeadingPage>
           <div className="flex flex-col gap-4">
-            {blogs.map((item, index) => (
-              <Link key={item._id} to={item.slug}>
-                <div className="bg-strock dark:bg-darkStroke rounded-lg p-5">
-                  <div className="flex flex-col gap-2">
-                    <h1 className="text-2xl font-semibold text-text1 dark:text-whiteSoft">
-                      {item.title}
-                    </h1>
-                    <p className="text-lg font-medium text-text1 dark:text-text3">
-                      {item.description}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ))}
+            {loading ? (
+              <div className="">
+                <div className="w-10 h-10 mx-auto border-4 border-t-4 rounded-full border-primary border-t-transparent animate-spin"></div>
+              </div>
+            ) : (
+              <>
+                {blogs.map((item, index) => (
+                  <Link key={item._id} to={item.slug}>
+                    <div className="bg-strock dark:bg-darkStroke rounded-lg p-5">
+                      <div className="flex flex-col gap-2">
+                        <div className="flex items-center justify-between">
+                          <h1 className="text-2xl font-semibold text-text1 dark:text-whiteSoft">
+                            {item.title}
+                          </h1>
+                          <span className="text-lg font-medium text-text1 dark:text-text3">
+                            {moment(item.updatedAt).format("DD/MM/YYYY")} (
+                            {`${moment(item.updatedAt).fromNow()}`})
+                          </span>
+                        </div>
+                        <p className="text-lg font-medium text-text1 dark:text-text3">
+                          {item.description}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </>
+            )}
           </div>
         </div>
       </div>
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -8,16 +8,18 @@ const PostPage = () => {
   const { blogSlug } = useParams();
 
   const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         `https://backendhongduccodedao-production.up.railway.app/api/v1/posts/${blogSlug}`
       )
       .then((res) => {
         if (res.status === 200) {
-          console.log(res.data);
           setPost(res.data);
+          setLoading(false);
         } else {
           console.log("error");
         }
@@ -28,23 +30,31 @@ const PostPage = () => {
     <Fragment>
       <div className="bg-lite dark:bg-darkbg h-screen lg:py-[100px] pt-[100px] pb-[150px]">
         <div className="max-w-[1200px] mx-auto px-5 lg:px-0">
-          <h1 className="text-white text-5xl font-bold">
-            {post[0] && post[0].title}
-          </h1>
-          <p className="text-text3 text-2xl font-medium">
-            {post[0] && post[0].description}
-          </p>
-          <div className="mt-5">
-            <span className="text-white">
-              Updated at:{" "}
-              {moment(post[0] && post[0].updatedAt).format("DD/MM/YYYY")} (
-              {`${moment(post[0] && post[0].updatedAt).fromNow()}`})
-            </span>
-          </div>
-          <div className="w-full h-[3px] bg-darkStroke rounded-full my-2"></div>
-          <div className="mt-5 text-whiteSoft overwrite-css">
-            {post[0] && parse(post[0].content)}
-          </div>
+          {loading ? (
+            <div className="w-full h-[70vh] flex items-center justify-center">
+              <div className="w-10 h-10 mx-auto border-4 border-t-4 rounded-full border-primary border-t-transparent animate-spin"></div>
+            </div>
+          ) : (
+            <>
+              <h1 className="text-white text-5xl font-bold">
+                {post[0] && post[0].title}
+              </h1>
+              <p className="text-text3 text-2xl font-medium">
+                {post[0] && post[0].description}
+              </p>
+              <div className="mt-5">
+                <span className="text-white">
+                  Updated at:{" "}
+                  {moment(post[0] && post[0].updatedAt).format("DD/MM/YYYY")} (
+                  {`${moment(post[0] && post[0].updatedAt).fromNow()}`})
+                </span>
+              </div>
+              <div className="w-full h-[3px] bg-darkStroke rounded-full my-2"></div>
+              <div className="mt-5 text-whiteSoft overwrite-css">
+                {post[0] && parse(post[0].content)}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </Fragment>
