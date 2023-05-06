50a70793e5f1e9bfbb3c663fe9d112f33e594d10
hongduccodedao <contact.hongduc@gmail.com>
2023-05-06 22:33:01 +0700

    🔥 refactor(Input.tsx): remove unused props The 'name' and 'control' props were not being used in the Input component, so they were removed to improve code readability and maintainability.

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -29,8 +29,6 @@ const Input = (props: InputProps) => {
   return (
     <input
       id={name}
-      name={name}
-      control={control}
       type={type}
       className={`border focus:border-primary outline-none bg-transparent rounded-md caret-text2 text-text4 px-4 py-2 placeholder:text-text4 dark:placeholder:text-text2 ${
         error.length > 0 ? "border-error" : "border-text2"
