e3e311fa6df28bc29fa33c15c85364fd43248b01
hongduccodedao <contact.hongduc@gmail.com>
2023-10-15 18:10:08 +0700

    Update image to anime

--- pages/api/imageToAnime.ts
+++ pages/api/imageToAnime.ts
@@ -32,6 +32,6 @@ export default async function handler(
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json("Something went wrong");
+    res.status(500).json(error);
   }
 }
