edff229c137a26785fa604c845095a69fc62cf0f
hongduccodedao <contact.hongduc@gmail.com>
2023-10-15 11:41:02 +0700

    Update chat simsimi

--- pages/api/chatsimsimi.ts
+++ pages/api/chatsimsimi.ts
@@ -1,21 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { text } = req.body;
-  const response = await fetch("https://api.simsimi.vn/v1/simtalk", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: JSON.stringify({
-      text: text,
-      lc: "vn",
-    }),
-  });
+  try {
+    const { text } = req.body;
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-  const data = await response.json();
-  res.status(200).json({ data });
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("text", text);
+    urlencoded.append("lc", "vn");
+
+    const requestOptions: RequestInit = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    const response = await fetch(
+      "https://api.simsimi.vn/v1/simtalk",
+      requestOptions
+    );
+    const result = await response.text();
+    console.log(result);
+
+    const data = { result }; // Assuming you want to return the result as 'data'
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
