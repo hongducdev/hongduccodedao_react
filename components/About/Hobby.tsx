3d7e2aa5a72d2ff2b2e0a2430d6265251ffd4462
hongduccodedao <contact.hongduc@gmail.com>
2023-11-06 17:33:34 +0700

    Responsive About Page

--- components/About/Hobby.tsx
+++ components/About/Hobby.tsx
@@ -6,10 +6,12 @@ const Hobby: React.FC<IHobby> = ({ title, description, tags }) => {
   return (
     <div>
       <h3 className="font-semibold text-3xl mb-2 text-ctp-subtext0">{title}</h3>
-      <p className="mb-2 text-ctp-overlay2 text-xl">{description}</p>
+      <p className="mb-2 text-ctp-overlay2 text-lg md:text-xl">{description}</p>
       <div className="flex items-center gap-3 flex-wrap">
         {tags.map((tag) => (
-          <Badge key={tag.name} className="text-base bg-ctp-green">{tag.name}</Badge>
+          <Badge key={tag.name} className="text-sm md:text-base bg-ctp-green">
+            {tag.name}
+          </Badge>
         ))}
       </div>
     </div>
