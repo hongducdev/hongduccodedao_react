df41e9047e33f4d76cbf7d0612b939304fd4c23d
hongduccodedao <contact.hongduc@gmail.com>
2023-11-07 21:16:30 +0700

    Update hover social icon

--- components/Header/Social.tsx
+++ components/Header/Social.tsx
@@ -1,31 +1,20 @@
 import { SocialList } from "@/utils/SocialList";
 import React from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import Link from "next/link";
 
 const Social = () => {
   return (
     <div className="flex items-center gap-5">
       {SocialList.map((item) => (
-        <TooltipProvider key={item.url}>
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href={item.url} className="hover:text-ctp-green">
-                {item.icon}
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent className="bg-ctp-base">
-              <p>
-                {item.label} - {item.username}
-              </p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <a
+          href={item.url}
+          className="hover:text-ctp-green"
+          key={item.url}
+          title={`${item.label}: ${item.username}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {item.icon}
+        </a>
       ))}
     </div>
   );
