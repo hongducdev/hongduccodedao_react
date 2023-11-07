c295babb5d8f8b723fbdebf67e4cf657bd4d513f
hongduccodedao <contact.hongduc@gmail.com>
2023-11-07 20:25:09 +0700

    Update SEO Project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,12 @@
 import Project from "@/components/Project";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Projects | hongduccodedao",
+  description:
+    "These are all projects that have been accumulated through the process of studying and working in groups with friends. All are publicly available on the open source Github: hongduccodedao.",
+};
 
 const ProjectsPage = () => {
   return <Project />;
