4a95eb77c402baeb46a3d78c548c8b194f34e32f
hongduccodedao <contact.hongduc@gmail.com>
2023-11-07 14:32:18 +0700

    Update project

--- components/Project/index.tsx
+++ components/Project/index.tsx
@@ -23,37 +23,30 @@ interface ProjectDataProps {
   updated_at: string;
 }
 
+const getProjects = async () => {
+  const response = await axios.get(
+    "https://api.github.com/users/hongduccodedao/repos?per_page=100"
+  );
+  if (response.status === 200) {
+    const data = response.data;
+    return data;
+  } else {
+    toast({
+      title: "Uh oh! Something went wrong.",
+      description: "There was a problem with your request.",
+    });
+  }
+};
+
 const ProjectsPage: React.FC = () => {
-  const [projectData, setProjectData] = useState<ProjectDataProps[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const [searchResults, setSearchResults] = useState<ProjectDataProps[]>([]);
+  const [projectData, setProjectData] = useState<ProjectDataProps[]>([]);
 
   useEffect(() => {
-    const getProjects = async () => {
-      const response = await axios.get(
-        "https://api.github.com/users/hongduccodedao/repos?per_page=100"
-      );
-      if (response.status === 200) {
-        const data = response.data;
-        setProjectData(data);
-        setSearchResults(data);
-      } else {
-        toast({
-          title: "Uh oh! Something went wrong.",
-          description: "There was a problem with your request.",
-        });
-      }
-    };
-
-    getProjects();
+    getProjects().then((data) => {
+      setProjectData(data);
+    });
   }, []);
-
-  useEffect(() => {
-    const filteredProjects = projectData.filter((project) =>
-      project.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(filteredProjects);
-  }, [searchTerm, projectData]);
 
   return (
     <div className="max-w-[1200px] w-full mx-auto mt-5 mb-20 md:my-10 flex flex-col gap-5 px-2 min-h-[calc(100vh-80px)]">
@@ -67,16 +60,13 @@ const ProjectsPage: React.FC = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-10">
-          {searchResults.length === 0 ? (
-            <p>
-              No projects found with keyword{" "}
-              <span className="font-medium">{searchTerm}</span>
-            </p>
-          ) : (
-            searchResults.map((project) => (
+          {projectData
+            .filter((project) =>
+              project.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((project) => (
               <ProjectItem key={project.id} project={project} />
-            ))
-          )}
+            ))}
         </div>
       </div>
     </div>
